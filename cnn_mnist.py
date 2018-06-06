4ff52d1a0ab83c1a058edf6d2b3666131d1d84e4
wishprophet <410143748@qq.com>
2018-06-06 09:13:21 +0800

    cnn_on_mnist

---
+++ cnn_mnist.py
@@ -0,0 +1,14 @@
+# encoding: utf-8
+
+"""
+@version: v1.0
+@author: Mouse
+@license: Apache Licence 
+@contact: admin@lovexing.cn
+@software: PyCharm
+@file: cnn_mnist.py
+@time: 18-6-6 上午9:12
+"""
+
+if __name__ == "__main__":
+    pass
